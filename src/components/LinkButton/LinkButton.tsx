4242aae37c8e770b9e1f3fcbae7e6486e6c99117
Misha Goncharov <forsomeletters@gmail.com>
2024-01-25 01:34:20 +0100

    Create the `LinkButton` component

---
+++ src/components/LinkButton/LinkButton.tsx
@@ -0,0 +1,28 @@
+import styles from './LinkButton.module.scss'
+import { MouseEvent } from 'react'
+import Link from 'next/link'
+
+/** Properties of the `LinkButton` component */
+type LinkButtonProps = {
+    label: string,
+    href: string,
+    onClick?: ((event: MouseEvent<HTMLElement>) => void)
+}
+
+/**
+ * A Next.js `Link` wrapper that resembles a button
+ * @component
+ * @param {LinkButtonProps} props A list of properties for the button
+ */
+export default function LinkButton({ label, href, onClick }: LinkButtonProps) {
+    return (
+        <Link
+            role='button'
+            href={href}
+            onClick={onClick}
+            className={styles.link}
+        >
+            {label}
+        </Link>
+    )
+}
